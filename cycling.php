f31bb5f7d68b1efdcb0dfd18bceea81f1338fc5b
Rachel Weissmandl <contactmeatrachelw@gmail.com>
2023-11-02 17:41:04 +0000

    map included in loop, yay!

--- cycling.php
+++ cycling.php
@@ -54,58 +54,54 @@
                 <p>No better way to experience the joys of a project's fruition, when it always reminds of the simple joys of cycling. I am learning to program by displaying my cycling activities. <a href="https://github.com/RWeissmandl/Pulling_Strava_Activities">Here is the code</a>.</p>
             </section>
 
+<script> function loadMap(encodedMapString, activityTitle, varName) {
+    let polyline = L.Polyline.fromEncoded(encodedMapString); //getting the summary polyline string and converting to lat/long
+    let coordinates = L.Polyline.fromEncoded(encodedMapString).getLatLngs(); //storing above as lat/long
+    let object_1 = polyline.getLatLngs()[0]; //grabbing fifth lat/long object
+    //let map = L.map('map').fitBounds(coordinates); //initialising map
+    let map = L.map(varName).fitBounds(coordinates); //initialising map
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    }).addTo(map); //adding openstreetmap layer
+    L.polyline(coordinates, {color:"red"}).addTo(map); //adding route to map
+    // Adding popup with Name of Activity at starting point
+    L.marker(Object.values(object_1)).addTo(map)
+    .bindPopup(activityTitle)
+    .openPopup();
+}
+</script>
+
 <!-- Connecting to db with PHP -->
-            <?php
+<?php
 
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 $db_connection = pg_connect("host=".$db["host"]." port=".$db["port"]." dbname=".$db["path"]." user=".$db["user"]." password=".$db["pass"]);
-$result = pg_query($db_connection, "select * from activities where (Sport_type='Ride' and Distance > 44) OR (Sport_type='Run' and Distance > 6) or (Sport_type='Hike' and Distance > 10)");
+$result = pg_query($db_connection, "select * from activities where (Sport_type='Ride' and Distance > 46) OR (Sport_type='Run' and Distance > 6) or (Sport_type='Hike' and Distance > 14)");
 
-// while ($row = pg_fetch_array($result)) {
-// echo $row[0].$row[1].$row[2].$row[3].$row[4].$row[5].$row[6].$row[7].$row[8];
-// echo "<br />\n";
-// }  
+$x=0;
 
-?>
-
-    <?php
-        while($row = pg_fetch_row($result)) {
-           echo "<table>";
-            echo "<tr><td>$row[0]</td></tr>";
-            $activity_title = "$row[0]";
-            echo "<tr><td>$row[6]</td></tr>";
-            echo "<tr><td>$row[1]miles</td></tr>";
-            echo "<tr><td>$row[2]seconds</td></tr>";
-            echo "<tr><td>$row[3]meters</td></tr>";
-            echo "<tr><td>$row[8]average speed</td></tr>";
-            $php_map = "$row[7]";
-            $escaped_php_map = str_replace('\\', '\\\\', $php_map);
-           // echo "<tr><td><script> document.write($row[7]) </script></td></tr>";
-            // echo "<tr><td><script> js_map = $php_map; document.write(js_map) </script></td></tr>";
-            // echo "<tr><td>$map_variable</td></tr>"; 
-            echo "</table>";
-        }
-        pg_close($db_connection); #CLOSE CONNECTION 
-        ?>    
-   
-
-<div id="map"><script>
-var polyline = L.Polyline.fromEncoded('<?php echo $escaped_php_map; ?>'); //getting the summary polyline string and converting to lat/long
-let coordinates = L.Polyline.fromEncoded('<?php echo $escaped_php_map; ?>').getLatLngs(); //storing above as lat/long
-let object_1 = polyline.getLatLngs()[0]; //grabbing fifth lat/long object
-//var map = L.map('map').setView(Object.values(object_1), 13); //initialising map using the first lat/long object
-let map = L.map('map').fitBounds(coordinates); //initialising map
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-}).addTo(map); //adding openstreetmap layer
-L.polyline(coordinates, {color:"red"}).addTo(map); //adding route to map
-// Adding popup with Name of Activity at starting point
-L.marker(Object.values(object_1)).addTo(map)
-    .bindPopup('<?php echo $activity_title; ?>')
-    .openPopup();
-</script></div>
+while($row = pg_fetch_row($result)) {
+    echo "<table class='table'>";
+    $activity_title = "$row[0]";
+    echo "<tr><td>Distance: $row[1]miles</td></tr>";
+    $distance = "$row[1]";
+    echo "<tr><td>Moving Time: $row[2]</td></tr>";
+    $moving_time = "$row[2]";
+    echo "<tr><td>Elevation: $row[3]feet</td></tr>";
+    echo "<tr><td>$row[8]mph</td></tr>";
+    echo "<tr><td>Date: $row[6]</td></tr>";
+    echo "</table>";
+    $php_map = "$row[7]";
+    $escaped_php_map = str_replace('\\', '\\\\', $php_map);
+    echo "<div class='map' id=\"map$x\"></div>";
+    echo "<script>loadMap(\"$escaped_php_map\", \"$activity_title\", \"map$x\")</script>";
+    $x++;
+    echo "</br></br>";
+}
+pg_close($db_connection); #CLOSE CONNECTION 
+?>    
 
 </body>
 

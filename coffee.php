865c1128463c1c40d161d2eba495dc76bbd87b29
Rachel Weissmandl <contactmeatrachelw@gmail.com>
2023-12-21 13:38:38 +0000

    fixed header margin

---
+++ coffee.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>coffee</title>
+    <!-- <link rel="stylesheet" href="reset.css"> -->
+    <link rel="stylesheet" href="coffee.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- including leaflet -->
+    
+    <!-- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossorigin=""/>
+     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+     crossorigin=""></script>
+     <script src="res/Polyline.encoded.js"></script> -->
+</head>
+
+<!-- including template header and styling -->
+<?php require_once('header.php'); ?>
+
+<body>
+    <div class='content'>
+        <main>
+        <section>
+                <h1>Coffee</h1>
+                <p class='intro'>
+                I attribute my successes to coffee, indeed, "I run on coffee and insulin". 
+                I became more invested in coffee when I realised that it would be cheaper to buy a machine and learn how to make
+                good coffee at home. At that point, my definition of good was still Starbucks, and I was buying a coffee there
+                each day on the way to work (and sometimes on its return). And thus, coffee became more than simply a drink,
+                but a hobby. These days, I find independent cafes wherever I can, and take pride in reviewing them. 
+                </br></br>
+                <b> Coming soon: </b> I am now working on setting up a page for these reviews. 
+                In the meantime, here is my own little home coffee setup.
+                </p>
+                <img src="res/coffeesetup.jpg" alt="coffee setup">
+        </section>
+    </div>
+</body>
+
---
+++ research.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>educational_research</title>
+    <!-- <link rel="stylesheet" href="reset.css"> -->
+    <link rel="stylesheet" href="research.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<!-- including template header and styling -->
+<?php require_once('header.php'); ?>
+
+<body>
+    <div class='content'>
+        <main>
+        <section>
+                <h1>Research</h1>
+                <p class='intro'>
+                My area of research interest lies in student voice in primary education. I am particularly interested in how mainstream education 
+                can grant students choice in what and how they learn. My most recent research, a Master dissertation for the
+                University of Cambridge considers this from the perspectives of parents and young people attending self-directed or democratic
+                settings. It explores the question philosophically, drawing on UNCRC's Article 3 vs Article 12, perspectives of childhood, 
+                and its relationship with how children learn. I am also focussed on research methodology and how it relates to 
+                epistemological and ontological viewpoints.
+                I am open for oppertunities for collaberative research in this and all matters surrounding education. 
+                </br></br>
+                <a href="">This is a copy of my full dissertation</a>
+                </br>
+                Soon to come: A userfriendly summary of my dissertation. 
+                </br></br>
+                <b>Previous topics for PGCE and university application, include</b>:
+                </br>
+                </br>Controversy of faith schools in Britain
+                </br>Role of research in developing primary education policy and practice
+                </br>Extent to which children should be given voice in primary education
+                </br>Effective approaches and challenges to creativity in primary schools
+
+
+                
+
+
+
+
+
+        </section>
+    </div>
+</body>
+
